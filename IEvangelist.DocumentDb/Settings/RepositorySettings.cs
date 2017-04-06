1d60240141bd4a567e39642830a94c0c9b7e93bd
David Pine <david.pine.7@gmail.com>
2017-04-05 22:48:20 -0500

    Added folders and settings for repo

---
+++ IEvangelist.DocumentDb/Settings/RepositorySettings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace IEvangelist.DocumentDb.Settings
+{
+    public class RepositorySettings : IOptions<RepositorySettings>
+    {
+        public string Endpoint { get; set; }
+
+        public string Key { get; set; }
+
+        public string DatabaseId { get; set; }
+
+        public string CollectionId { get; set; }
+
+        RepositorySettings IOptions<RepositorySettings>.Value => this;
+
+        public Uri GetDocumentCollectionUri()
+            => UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId);
+
+        public Uri GetDocumentUri(string id)
+            => UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id);
+    }
+}
